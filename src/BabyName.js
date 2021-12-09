69ea07872ae01a3a7fdf8fe0e6d4b09869716889
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-12-09 20:38:53 +0000

    Added search bar

--- src/BabyName.js
+++ src/BabyName.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BabyNames = (prop) => (
-    <div className="baby-names-container" >{
-        prop.namesOfBaby.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase())).map((babyName) => {
-            return <span className={`all-names ${babyName.sex === "m" ? "male" : "female"}`} key= {babyName.id} >{babyName.name} </span>;
+
+const BabyNames = (prop) => {
+const [searchTerm, setSearchTerm] = useState("");
+
+   return <div className="baby-names-container" >
+        <input
+        className="search" placeholder="Search baby names.."
+        onChange={(e)=> setSearchTerm(e.target.value)}
+        type="text" />
+        {
+        prop.namesOfBaby.sort((a, b) =>
+        a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
+        .filter((babyName) => {
+            if (searchTerm === "") {
+                return babyName;
+            } else if (babyName.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return babyName;
+            }
+            return false;
         })
-        }<hr/> </div>
-);
+        .map((babyName) => {
+            return <span className={`all-names ${babyName.sex === "m" ? "male" : "female"}`}
+            key= {babyName.id}
+            onClick={favourite} >{babyName.name} </span>;
+        })
+        }<hr/>
+    </div>;
+};
 
 export default BabyNames;
---
+++ src/SearchBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const searchHandler = () => {
+
+};
+const SearchBar = () => {
+    return <input onKeyDown={searchHandler} type="text"/>;
+};
